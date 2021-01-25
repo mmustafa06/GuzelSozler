9cd43862e6634a1ccfcd7921733ece08e4d25065
Mustafa <muhammedmustafaodabasi@gmail.com>
2021-01-25 19:28:47 +0100

    v 0.2

--- index.js
+++ index.js
@@ -121,6 +121,11 @@ app.get("/", function (req, res) {
 
 
 
-app.listen(5000, () => {
-    console.log("5000 portuna baglandik")
-})
+let port = process.env.PORT;
+if (port == "" || port == null) {
+    port = 5000;
+}
+
+app.listen(port, function () {
+    console.log("port numarasi: " + port);
+});
